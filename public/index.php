31fac60cbb99253056cabb8a9597aaf2ee3076b1
Prince Asiedu <princekofasiedu@gmail.com>
2023-09-05 19:18:59 +0000

    Feature transaction extraction
    Included dollar amount formatting

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR );
 define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
+require APP_PATH . 'helpers.php';
 require APP_PATH . 'App.php';
 
 $files = getTransactionFiles(FILES_PATH);
